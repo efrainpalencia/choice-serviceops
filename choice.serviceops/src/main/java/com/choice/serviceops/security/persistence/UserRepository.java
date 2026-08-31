3b30514fc069120e4260375f6955d90d8fcc8ca7
Efrain Palencia <efrainpalencia@hotmail.com>
2026-08-31 19:30:00 -0400

    Builds user and role repositories

---
+++ choice.serviceops/src/main/java/com/choice/serviceops/security/persistence/UserRepository.java
@@ -0,0 +1,14 @@
+package com.choice.serviceops.security.persistence;
+
+import com.choice.serviceops.security.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmailIgnoreCase(String email);
+
+    boolean existsByEmailIgnoreCase(String email);
+
+}
